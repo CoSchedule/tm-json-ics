e88d6185f8d554958fa59275cc00f7ea52d2c317
jasonmosser <jason@coschedule.com>
2015-04-14 13:22:54 -0500

    Updated to output VCALENDAR version 2.0. Escaping text fields. Other minor cleanup.

--- index.js
+++ index.js
@@ -2,114 +2,131 @@ var _ = require('underscore')._;
 var moment = require('moment');
 
 module.exports = function(config) {
-    this.file = "";
 
     var toUTC = function(unixTimestamp) {
-            //javescript uses millisecs, not seconds since the epoch.
-            timestamp = parseInt(unixTimestamp) * 1000;
-            date = moment(timestamp).utc();
-            return date.format('YYYYMMDDTHHmmss[Z]')
-        };
+        //javascript uses milliseconds, not seconds since the epoch.
+        var timestamp = parseInt(unixTimestamp) * 1000;
+        var date = moment(timestamp).utc();
+        return date.format('YYYYMMDDTHHmmss[Z]')
+    };
 
     var addLine = function(title, line) {
+        var buffer = '';
 
-            if (line === undefined || line === null) {
-                return;
+        if (line === undefined || line === null) {
+            return buffer;
+        }
+
+        if (title.length + 1 + line.length <= 73) {
+            buffer += title + ':' + line.substr(0, 73) + '\r\n';
+        } else {
+            buffer += title + ':';
+            var maxLen = 73 - title.length + 1;
+
+            while (line.length >= maxLen) {
+                //The space the the end of this string is SUPER-IMPORTANT!
+                buffer += line.substr(0, maxLen) + '\r\n' + ' ';
+                line = line.substr(maxLen);
+                //We need to adjust the maxLen the first time we go through this
+                //loop to accomidate the length of the title.  After the forst iteration,
+                //we need to set it back to it's original value (73)
+                maxLen = 73;
             }
-
-            if (title.length + 1 + line.length <= 73) {
-                this.file += title + ":" + line.substr(0, 73) + "\r\n";
-            } else {
-                this.file += title + ":";
-                var maxlen = 73 - title.length + 1;
-
-                while (line.length >= maxlen) {
-                    //The space the the end of this string is SUPER-IMPORTANT!
-                    this.file += line.substr(0, maxlen) + "\r\n" + " ";
-                    line = line.substr(maxlen);
-                    //We need to adjust the maxlen the first time we go through this 
-                    //loop to accomidate the length of the title.  After the forst iteration,
-                    //we need to set it back to it's original value (73)
-                    maxlen = 73;
-                }
-                this.file += line.substr(0, 73) + "\r\n";
-            }
-        };
+            buffer += line.substr(0, 73) + '\r\n';
+        }
+        return buffer;
+    };
 
     var createICS = function(config) {
-            this.file = "";
+        var buffer = '';
 
-            addLine("BEGIN", "VCALENDAR");
-            addLine("PRODID", "-//TODAYMADE//JSONICS//EN");
-            addLine("VERSION", "1.0");
-            addLine("CALSCALE", "GREGORIAN");
+        buffer += addLine('BEGIN', 'VCALENDAR');
+        buffer += addLine('VERSION', '2.0');
+        buffer += addLine('PRODID', '-//COSCHEDULE//COSICS//EN');
+        buffer += addLine('CALSCALE', 'GREGORIAN');
 
-            // Calendar Name
-            if (config.title !== undefined) {
-                addLine("X-WR-CALNAME", config.title);
-            }
+        // Calendar Name
+        if (config.title !== undefined) {
+            buffer += addLine('X-WR-CALNAME', escapeText(config.title));
+        }
 
-            processJSONEvents(config.events);
+        buffer += processJSONEvents(config.events);
 
-            addLine("END", "VCALENDAR")
-            return this.file;
-        };
+        buffer += addLine('END', 'VCALENDAR');
+
+        return buffer;
+    };
 
     var processJSONEvents = function(events) {
-            var self = this;
-            _.each(events, function(evt) {
+        var buffer = '';
+        _.each(events, function(evt) {
 
-                var skip = false;
-                if (evt.id === undefined) {
-                    console.log("Must Specify a unique id.")
-                    skip = true;
-                }
-                if (evt.start === undefined) {
-                    console.log("Must Specify a start date/time.")
-                    skip = true;
-                }
+            var skip = false;
+            if (evt.id === undefined) {
+                console.log('Must Specify a unique id.')
+                skip = true;
+            }
+            if (evt.start === undefined) {
+                console.log('Must Specify a start date/time.')
+                skip = true;
+            }
 
-                if (!skip) {
+            if (!skip) {
 
-                    addLine("BEGIN", "VEVENT");
-                    addLine("UID", evt.id);
+                buffer += addLine('BEGIN', 'VEVENT');
+                buffer += addLine('UID', evt.id);
 
-                    addLine("SUMMARY", evt.summary);
-                    addLine("DESCRIPTION", evt.description);
-                    addLine("DTSTAMP", toUTC(moment().unix()));
-                    addLine("DTSTART", toUTC(evt.start));
-                    if (evt.end == undefined) {
-                        //If there isn't an end time, assume it's an instantaneous
-                        //event, and have it end one second after it starts. 
-                        addLine("DTEND", toUTC(evt.start + 1));
-                    } else {
-                        addLine("DTEND", toUTC(evt.end));
-                    }
-                    addLine("CATEGORIES", evt.categories);
-                    addLine("URL", evt.url);
-                    addLine("END", "VEVENT");
+                buffer += addLine('SUMMARY', escapeText(evt.summary));
+                buffer += addLine('DESCRIPTION', escapeText(evt.description));
+                buffer += addLine('DTSTAMP', toUTC(moment().unix()));
+                buffer += addLine('DTSTART', toUTC(evt.start));
+                if (evt.end == undefined) {
+                    //If there isn't an end time, assume it's an instantaneous
+                    //event, and have it end one second after it starts.
+                    buffer += addLine('DTEND', toUTC(evt.start + 1));
                 } else {
-                    console.log("ERROR: ", evt);
+                    buffer += addLine('DTEND', toUTC(evt.end));
                 }
-            });
-        };
+                buffer += addLine('CATEGORIES', evt.categories);
+                buffer += addLine('URL', evt.url);
+                buffer += addLine('END', 'VEVENT');
+            } else {
+                console.log('ERROR: ', evt);
+            }
+        });
+        return buffer;
+    };
+
+    var escapeText = function(s) {
+        var t;
+        if (s == null) {
+            return s;
+        }
+        t = s.replace(/([,;\\])/ig, '\\$1');
+        t = t.replace(/\n/g, '\\n');
+        return t;
+    };
 
     return createICS(config);
 };
 
-// json = {
+//var json = {
 //     events: [{
 //      'id':'123456@todaymademyid',
-//         "url": "http://www.todamade.com",
-//         "summary": "Tweet: [HeadLine]",
-//         "start": moment("2013-03-14 12:30:00", "YYYY-MM-DD HH:mm:ss").unix(),
+//         'url': 'http://www.todamade.com',
+//         'summary': 'Tweet: [HeadLine] {April 6, 2015} (Meal Planning Magic)',
+//         'start': moment('2013-03-14 12:30:00', 'YYYY-MM-DD HH:mm:ss').unix(),
 //         'categories': 'TWEET',
-//         "description": "Maybe we put the tweet in here? Man this is such a long post it's just so many characters I can't even believe how many charaters there are here. \n Who even types so many characters?"
+//         'description': 'Maybe we put the tweet in here? Man this is such a long post it's just so many characters I can't even believe how many characters there are here. \n Who even types so many characters?'
 //     }, {
 //      'id':'123457@todaymademyid',
-//         "summary": "Post: [Headline]",
-//         "start": moment("2013-03-14 12:30:00", "YYYY-MM-DD HH:mm:ss").unix(),
+//         'summary': 'Post: [Headline]',
+//         'start': moment('2013-03-14 12:30:00', 'YYYY-MM-DD HH:mm:ss').unix(),
 //         'categories': 'BLOG',
-//         "description": "Maybe we put the post in here?"
+//         'description': 'Maybe we put the post in here?'
 //     }]
-// }
+//};
+//
+//console.log(module.exports(json));
+
+
